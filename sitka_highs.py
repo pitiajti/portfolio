ee5cef3e13579332cc60cbd4ef878625ff92626c
pitiajti <pitiajti@gmail.com>
2023-08-02 14:26:21 +0200

    updating sitka_highs.py

--- sitka_highs.py
+++ sitka_highs.py
@@ -5,4 +5,15 @@ lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
+
+# Extract high temperatures.
+highs = []
+for row in reader:
+    high = int(row[4])
+    highs.append(high)
+
+print(highs)
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
 print(header_row)
